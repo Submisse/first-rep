731999cc241f6564da0df4ba07095b67d6a8559b
Sergey <submisse@gmail.com>
2018-07-07 10:52:10 +0400

    Верстка макета с помощью React

--- src/App.js
+++ src/App.js
@@ -3,31 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './components/header';
+import Menu from './components/menu';
+import Main from './components/main';
+
+const url = process.env.PUBLIC_URL + '/img/background.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-              <div className="wrapper">
-                <SayMyName name="Han" surname="Yolo" link="1#" />
-                <SayMyName name="Yasha" surname="Miroslavovich" link="2#" />
-                <SayMyName name="Kim" surname="Jentyrgaev" link="#" />
-              </div>
+        <div className="App">
+          <HeaderWrapper>
+            <Grid>
+              <Header />
+            </Grid>
+          </HeaderWrapper>
+          <MenuWrapper>
+            <Grid>
+              <Menu />
+            </Grid>
+          </MenuWrapper>
+          <MainWrapper>
+            <Grid>
+              <Main />
+            </Grid>
+          </MainWrapper>
+        </div>
     );
   }
 }
 
-function SayMyName(props) {
-  return (
-      <div>
-        <strong>Hello World!</strong>
-        <h1> Просто имя {props.name}, ну и фамилия - {props.surname}</h1>
-        <a href={props.link}> Ссылка на заглушку =) </a>
-      </div>
-    )
-}
 
 
 export default App;
